843775f9bfcc0e5401894deecfb6f7209dcee79e
Nnhuy6935 <101635126+Nnhuy6935@users.noreply.github.com>
2024-10-26 12:34:22 +0700

    check setup ci/cd

--- calculate/src/main/java/com/example/calculate/CalculateApplication.java
+++ calculate/src/main/java/com/example/calculate/CalculateApplication.java
@@ -16,7 +16,7 @@ public class CalculateApplication {
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+		return String.format("Hello %s! And Nice to meet you", name);
 	}
 
 }
